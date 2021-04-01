afb10f3aaa1c1587a4e6773d497ae442f8ee0099
ERic Echemane <eechemane29@gmail.com>
2021-04-01 14:48:17 +0800

    Enhanced shop items display cards

--- src/components/CoffeeProduct.tsx
+++ src/components/CoffeeProduct.tsx
@@ -1,28 +1,32 @@
 import {Grid} from "@material-ui/core";
 import StarIcon from '@material-ui/icons/Star';
-import StarHalfIcon from '@material-ui/icons/StarHalf';
+import StarRateIcon from '@material-ui/icons/StarRate';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Tooltip from '@material-ui/core/Tooltip';
 
 export default function CoffeeProduct(props: any) {
     return (
         <Grid item xs={12} sm={6} md={4} lg={3} className="p-3">
-            <div className="bg-1 cur-pointer hover-lift trans-3 active-effect" title={props.desc}>
-                <img src={props.imgSrc} alt={props.name} className="fullWidth" />
-                    <h3 className="pl-2">{props.name}</h3>
-                    <p className="prime d-flex flex-align-center pl-2">
-                        <StarIcon fontSize="small" className="f-size-small" />
-                        <StarIcon fontSize="small" />
-                        <StarIcon fontSize="small" />
-                        <StarIcon fontSize="small" />
-                        <StarHalfIcon fontSize="small" />
-                        ({props.rating})
-                    </p>
-                    <div className="d-flex pl-2 pt-2 pb-2">
+            <div className="bg-1 cur-pointer hover-lift trans-3 active-effect">
+                <img src={props.imgSrc} alt={props.name} className="fullWidth" title={props.desc} />
+                    <h3 className="pl-2" title={props.desc}>{props.name}</h3>
+                    <Tooltip title={`${props.rating} people like this.`}>
+                        <p className="prime d-flex flex-align-center pl-2 pr-2">
+                            <div className="flex-1 pt-1">
+                                <StarRateIcon fontSize="small" />
+                                <StarRateIcon fontSize="small" />
+                                <StarRateIcon fontSize="small" />
+                                <StarRateIcon fontSize="small" />
+                                <StarRateIcon fontSize="small" /> 
+                            </div>
+                            <span>{props.rating}</span>
+                        </p>
+                    </Tooltip>
+                    <div className="d-flex pl-2 pt-2 pb-1">
                         <h3 className="second flex-1">$ {props.price}</h3>
                     </div>
-                    <div className="pl-2 pb-2 d-flex">
-                        <p className="cur-pointer flex-1 hover-blue f-size-small">Buy now</p>
+                    <div className="pl-2 pb-2 d-flex">  
+                        <p className="cur-pointer flex-1 hover-blue f-size-small">Order now</p>
                         <Tooltip title="Add to cart" placement="left-end">
                             <span 
                                 onClick={props.handleAddToCart}
